8deeb00674fcfe71f5edbace8218b82348eaf77f
Romain Menke <romainmenke@gmail.com>
2015-11-04 16:51:12 +0100

    Initial Commit

---
+++ CV/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CV
+//
+//  Created by Romain Menke on 04/11/15.
+//  Copyright © 2015 Romain Menke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
